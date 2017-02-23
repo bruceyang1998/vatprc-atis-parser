c69fa8b33d2a4c9d1e39db60f303d0f37e5fb806
Bruce Yang <bruceyang1998@gmail.com>
2017-02-23 11:10:08 -0500

    removed Hello World

--- atis.php
+++
@@ -1,4 +0,0 @@
-<?php
-Echo '[' . $_GET["arpt"] . '][information]' . $_GET["info"] . ' ' . substr($_GET["metar"], 7, 4) . '[UTC][DEPRWY]' . $_GET["dep"] . '[LDGRWY]' . $_GET["arr"] . '[EXPECT][' . $_GET["apptype"] . "][APP][RWY]" . $_GET["arr"] . '[wind]' . substr($_GET["metar"], 13,3) . '[degrees][at]' . substr($_GET["metar"], 16,2) . '[MPS]';
-//Echo 'Hello, world! You entered: Arrival: ' . $_GET["arr"] . ' Departure: ' . $_GET["dep"] . ' Approach Type: ' . $_GET["apptype"] . ' Information: ' . $_GET["info"] . ' METAR: ' . $_GET["metar"] . ' Airport: ' . $_GET["arpt"] . ' Thank you for using atis.gq!';
-?>
